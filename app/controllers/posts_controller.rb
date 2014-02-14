d66922360d1e4dff0e90be87ade3e6373eb92332
John Doe <Neon_dvs@mail.ru>
2014-02-14 16:35:43 +0200

    метод класса Post работает нормально, также была сгенерированна модель Post но ещё не вносились изменения

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,24 @@
 class PostsController < ApplicationController
   def new
   end
+
+   def create
+   	render :text => params[:post].inspect
+  end
+
+   def show
+  end
+
+   def index
+  end
+
+   def destroy
+  end
+
+def edit
+  end
+
+   def update
+  end
+
 end
