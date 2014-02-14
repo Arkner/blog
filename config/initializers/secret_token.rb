7a9e460d6d732cf20adc6f7c13b9750763271998
John Doe <Neon_dvs@mail.ru>
2014-02-14 14:30:14 +0200

    Добавление сгенерированных файлов в гит

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'c98ffdcf567b0afdd4b6fbd9a7178385be9fa895bcbfc1ca1fc105da04ce619bc158c724a17cb26f455c169b98bd5d9f6e30b19e0454a39a3dfb4a8a47673898'
